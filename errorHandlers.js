ae77903184a57bc65b52f6e5d0c584ecc49386a6
zviels <59054323+zviels@users.noreply.github.com>
2021-04-13 10:56:55 +0300

    Updated Error Handlers

--- errorHandlers.js
+++ errorHandlers.js
@@ -5,13 +5,13 @@ const handlePageNotFoundError = (req, res, next) => {
     const error = new Error('Page Not Found!');
     error.status = 404;
 
-    next(error);
+    res.status(404).render('page-not-found', { error });
 
 }
 
 // Handle 500 Errors
 
-const handleServerError = (error, req, res, next) => res.status(error.status || 500).send(error.message);
+const handleServerError = (error, req, res, next) => res.status(error.status || 500).render('error', { error });
     
 // Export Error Handlers
 
